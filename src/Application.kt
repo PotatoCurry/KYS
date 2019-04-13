fecb7629ce6f91ad53b1de96629b3f7fc710f14d
PotatoCurry <potatocurry@protonmail.com>
2019-04-12 22:52:55 -0500

    Initial code commit

---
+++ src/Application.kt
@@ -0,0 +1,84 @@
+package io.github.potatocurry
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import freemarker.cache.*
+import io.ktor.freemarker.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.features.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
+    val client = HttpClient(Apache) {
+    }
+
+    routing {
+        get("/") {
+            call.respondHtml {
+                body {
+                    h1 { +"Clements YES Program" }
+                    div("info") {
+                        p {
+                            strong { +"WE HAVE TEMPORALLY DISABLED THE YES HOUR QUERY. IF YOU WANT TO FIND OUT HOW MANY YES HOURS YOU HAVE PLEASE EMAIL US AT CLEMENTSYES@GMAIL.COM." }
+                        }
+                    }
+                }
+            }
+        }
+
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.red
+                }
+                p {
+                    fontSize = 2.em
+                }
+                rule("p.myclass") {
+                    color = Color.blue
+                }
+            }
+        }
+
+        get("/html-freemarker") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+    }
+}
+
+data class IndexData(val items: List<Int>)
+
+fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+    style(type = ContentType.Text.CSS.toString()) {
+        +CSSBuilder().apply(builder).toString()
+    }
+}
+
+fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+    this.style = CSSBuilder().apply(builder).toString().trim()
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
