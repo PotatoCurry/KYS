692032e2ad04a52fadfa022551f6567636bcec0b
PotatoCurry <potatocurry@protonmail.com>
2019-04-17 21:48:25 -0500

    Update tests

--- test/io/github/potatocurry/ApplicationTest.kt
+++ test/io/github/potatocurry/ApplicationTest.kt
@@ -1,32 +1,39 @@
 package io.github.potatocurry
 
-import io.ktor.http.*
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
 import kotlin.test.*
-import io.ktor.server.testing.*
 
 /** Basic tests to ensure database is operational and web server is responsive. */
 class ApplicationTest {
-    /** Basic unit tests. */
+    /** Tests if web server is responsive and is able to retrieve student data. */
     @Test
-    fun testRoot() {
+    fun testWeb() {
         withTestApplication({ module() }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
+                assertNotNull(response.content)
             }
             handleRequest(HttpMethod.Get, "/query").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
             }
             handleRequest(HttpMethod.Get, "/query/625783").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
+                assertNotNull(response.content)
+                assertTrue(response.content!!.contains("Damian Lall"))
                 assertNotEquals("The specified student with ID 625783 was not found.", response.content)
                 assertNotEquals("Error parsing ID 625783.", response.content)
             }
             handleRequest(HttpMethod.Get, "/query/000000").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
+                assertNotNull(response.content)
                 assertEquals("The specified student with ID 000000 was not found.", response.content)
             }
             handleRequest(HttpMethod.Get, "/query/abc").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
+                assertNotNull(response.content)
                 assertEquals("Error parsing ID abc.", response.content)
             }
         }
