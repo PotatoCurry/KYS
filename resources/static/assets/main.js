0f6adc5b2c5c4433278bcc344094078111574502
qwertyuiop888 <albertsun0@gmail.com>
2019-04-19 20:18:40 -0500

    6 digit textbox lmao

--- resources/static/assets/main.js
+++ resources/static/assets/main.js
@@ -31,3 +31,17 @@ function myFunction() {
               setInterval(displayNextImage, 5000);
       }
 */
+function limit(element, max_chars)
+{
+    if(element.value.length > max_chars) {
+        element.value = element.value.substr(0, max_chars);
+    }
+}
+function minmax(value, min, max) 
+{
+    if(parseInt(value) < min || isNaN(parseInt(value))) 
+        return 0; 
+    else if(parseInt(value) > max) 
+        return 100; 
+    else return value;
+}
